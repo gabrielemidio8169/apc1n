2bf21f0b38f55c36f38fbd4a703aeb527370455b
gabrielemidio81 <25727564-gabrielemidio81@users.noreply.replit.com>
2023-08-24 00:46:09 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,16 @@
+/*
+Autor: Gabriel Emidio
+Data: 23/08/23
+
+Progama que exibe Olá pessoal
+
+*/
+#include <stdio.h>
+#include <math.h>
+
+// funcao principal
+int main(){
+  printf("Ola pessoal!\n");
+  printf("A raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
